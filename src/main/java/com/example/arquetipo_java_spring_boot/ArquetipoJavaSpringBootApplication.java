954d42925b4913955db51587e39d4bac05f3232f
Hugo Sacaquirin <hsacaqui@pichincha.com>
2025-07-10 10:53:12 -0500

    add arquetype spring boot

---
+++ src/main/java/com/example/arquetipo_java_spring_boot/ArquetipoJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.arquetipo_java_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquetipoJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquetipoJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/arquetipo_java_spring_boot/ArquetipoJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arquetipo_java_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquetipoJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
